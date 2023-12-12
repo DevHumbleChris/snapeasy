33e0e95fe21b00106970a9f2fe956c934bbea010
DevHumbleChris <dev.humblechris254@gmail.com>
2023-12-12 18:04:35 +0300

    created the api route (screenshot)

---
+++ server/api/screenshot.get.ts
@@ -0,0 +1,76 @@
+import chromium from "chrome-aws-lambda";
+
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
+
+export default defineEventHandler(async (event) => {
+  const {
+    url,
+    type = "png",
+    download = false,
+    fullscreen = false,
+  } = getQuery(event);
+
+  const outPut = { type, fullPage: fullscreen };
+
+  if (!url) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "URL parameter is required",
+    });
+  }
+  let browser = null;
+
+  try {
+    browser = await getBrowserInstance();
+    let page = await browser.newPage();
+    await page.goto(url);
+    const screenshot = await page.screenshot(outPut);
+
+    if (download) {
+      return screenshot;
+    } else {
+      setResponseStatus(event, 200);
+      event.headers.set("Content-Type", `image/${type}`);
+    }
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: (error as any).message,
+    });
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return {
+    url,
+  };
+});
